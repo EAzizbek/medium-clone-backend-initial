e7f13846f5e03d08fea488a2276181794c8038c6
EAzizbek <138809274+Azizbek1308@users.noreply.github.com>
2024-09-03 15:17:58 +0500

    7th changes

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 import os
+from django_resized import ResizedImageField
 import uuid
 
 # Create your models here.
@@ -12,7 +13,7 @@ def file_upload(instance, filename):
 
 class CustomUser(AbstractUser):
     middle_name = models.CharField(max_length=30, blank=True, null=True)
-    avatar = models.ImageField(upload_to=file_upload, blank=True)
+    avatar = ResizedImageField(size=[300, 300], crop=['top', 'left'], upload_to=file_upload, blank=True)
 
     class Meta:
         db_table = "user"
