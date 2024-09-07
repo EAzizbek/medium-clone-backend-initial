fe34a705d95dc03d95d66a2e2180db7238b1a178
EAzizbek <138809274+Azizbek1308@users.noreply.github.com>
2024-09-07 15:49:54 +0500

    SQL index

--- users/models.py
+++ users/models.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core import validators
 from users.errors import BIRTH_YEAR_ERROR_MSG
+from django.contrib.postgres.indexes import HashIndex
 
 # Create your models here.
 def file_upload(instance, filename):
@@ -41,6 +42,14 @@ class CustomUser(AbstractUser):
         verbose_name = "User"
         verbose_name_plural = "Users"
         ordering = ["-date_joined"]
+
+        indexes = [
+            HashIndex(fields=['first_name'], name='%(class)s_first_name_hash_idx'),
+            HashIndex(fields=['last_name'], name='%(class)s_last_name_hash_idx'),
+            HashIndex(fields=['middle_name'], name='%(class)s_middle_name_hash_idx'),
+            models.Index(fields=['username'], name='%(class)s_username_idx'),
+        ]
+
         constraints = [
             models.CheckConstraint(  # tug'ilgan yil oralig'ini tekshirish uchun uchunchi variant
                 check=models.Q(birth_year__gt=settings.BIRTH_YEAR_MIN) & models.Q(
