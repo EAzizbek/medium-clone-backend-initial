088162d9afcf37731b80dcda08ff9b3a61c1c2ad
EAzizbek <138809274+Azizbek1308@users.noreply.github.com>
2024-10-03 14:13:48 +0500

    Articles api1

--- articles/models.py
+++ articles/models.py
@@ -26,5 +26,8 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        db_table = 'article'
+
     def __str__(self):
         return self.title
