17cdd8162a94e1f896d951a7c3535a693d9e5858
EAzizbek <138809274+Azizbek1308@users.noreply.github.com>
2024-10-03 14:57:55 +0500

    Articles Create and Read Api 2

--- articles/models.py
+++ articles/models.py
@@ -27,7 +27,7 @@ class Article(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = 'Articles'
+        db_table = 'article'
 
     def __str__(self):
         return self.title
